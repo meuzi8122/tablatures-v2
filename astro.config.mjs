8e731f96870625f9c8a166c70a8f19f1fc2b403a
meuzi8122 <kagiakete1234@gmail.com>
2025-04-28 20:54:24 +0900

    SSRについてコメント追記

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    // output: "static"がデフォルトで設定されている
+    //（デフォルトはSSG、特定のページをSSRに変更可。v4までは"hybrid"を指定する必要があったが、v5からは不要。）
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
