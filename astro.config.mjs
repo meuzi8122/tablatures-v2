eadd3d58a717f63ceaf3c51d8992a67a01b68aa8
meuzi8122 <kagiakete1234@gmail.com>
2025-04-29 01:48:08 +0900

    ssr用のvercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-    // output: "static"がデフォルトで設定されている
-    //（デフォルトはSSG、特定のページをSSRに変更可。v4までは"hybrid"を指定する必要があったが、v5からは不要。）
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  // output: "static"がデフォルトで設定されている
+  //（デフォルトはSSG、特定のページをSSRに変更可。v4までは"hybrid"を指定する必要があったが、v5からは不要。）
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
 });
